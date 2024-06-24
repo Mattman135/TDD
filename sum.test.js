fcf5ab26790175da2945c9b0869cdc3cf5e66cbe
Mattis Hallberg <skatemattis97@hotmail.com>
2024-06-24 11:14:42 +0200

    Added 4 unit test for add, subtract, divide, multiply functions and calculator object

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,7 @@
 const sum = require("./sum")
 const capitalize = require("./capitalize")
 const reverseString = require("./reverseString")
+const calculator = require("./calculator")
 
 test("adds 1+2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3)
@@ -24,4 +25,20 @@ test("return reverse string", () => {
 
 test("return reverse string", () => {
   expect(reverseString("Greetings from Earth")).toBe("htraE morf sgniteerG")
+})
+
+test("calculator should take two arguments and return the correct result of the function added", () => {
+  expect(calculator.add(1, 1)).toBe(2)
+})
+
+test("calculator should take two arguments and return the correct result of the function added", () => {
+  expect(calculator.subtract(1, 1)).toBe(0)
+})
+
+test("calculator should take two arguments and return the correct result of the function added", () => {
+  expect(calculator.divide(1, 1)).toBe(1)
+})
+
+test("calculator should take two arguments and return the correct result of the function added", () => {
+  expect(calculator.multiply(6, 11)).toBe(66)
 })
